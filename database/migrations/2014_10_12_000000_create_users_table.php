33f416b9bc80baa6240fb0e9d08891956b532c2a
Alejandro González Reyes <alexglzrys@gmail.com>
2020-01-25 17:38:30 -0600

    Generar seeders, factories y migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,7 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->text('bio');
             $table->rememberToken();
             $table->timestamps();
         });
